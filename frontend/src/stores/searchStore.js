17701cf87b7e871661806aa4371844cc59784edc
InkFeather <2728266077@qq.com>
2026-02-23 23:12:13 +0800

    轻微调整

--- frontend/src/stores/searchStore.js
+++ frontend/src/stores/searchStore.js
@@ -75,7 +75,7 @@ export const useSearchStore = defineStore('search', () => {
             /paths?$/i,       // 屏蔽所有以 path 结尾的 (install_path)
             /colors?$/i,      // 屏蔽所有以 color 结尾的 (color)
             'description','descriptions_by_version' ,'notes',    // 屏蔽描述 (太长，不适合 key:value 搜索，适合全文搜索)
-            'version','workshop_id','id','mod_id', 'path_hash','rules', 'disabled',
+            'version','id','mod_id', 'path_hash','rules', 'disabled',
             'file_size',
             // 'ignored_issues',
         ]
